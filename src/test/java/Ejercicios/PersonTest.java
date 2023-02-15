8a5098f1f1eb8d96e3f4c433b3d6e45fb0c9cb14
antonioortegas <antonioortega@uma.es>
2023-02-15 16:34:49 +0100

    setup and numeration

--- src/test/java/Ejercicios/PersonTest.java
+++ src/test/java/Ejercicios/PersonTest.java
@@ -7,11 +7,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
     /*
-    -
+    IMPLEMENTED TESTS
+    1.getters all return the correct value
+    2.class constructor is correctly implemented
+    3.
+    4.
+    5.
+    6.
+    7.
+    8.
+    9.
+
      */
 
+    Person person;
     @BeforeEach
     void setUp() {
+        person = new Person("Antonio", 21, "Male");
     }
 
     @AfterEach
